9cdf5b1e2cdde4495627a2359ceb4bd47b1811d4
xpadrex <xpadrex@gmail.com>
2018-07-06 21:07:23 -0400

    Added objects, code still needs improvement

--- location.h
+++
@@ -1,9 +0,0 @@
-/* 
- * location.h - declares external functions for the 'LOOK' and 'GO' commands
- */
-
-extern void execute_look(const char *noun);
-extern void execute_go(const char *noun);
-extern void move_player(const char *direction);
-extern int player_location;
-//extern location areas[];
--- object.h
+++
@@ -1,9 +0,0 @@
-/*
- * object.h - define the object structs for use in the maze-rpg game
- */
-
-typedef struct {
-    const char *description;
-    const char *tag;
-    struct location *location;
-} object;
